560a6ed913390e833d538eebacfa67e4f5891c8b
Venkata Rami Reddy <venkataramireddy86@gmail.com>
2019-11-05 10:33:54 +0530

    initial commit

---
+++ ODataComlextype/App_Start/WebApiConfig.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+using Microsoft.AspNet.OData.Builder;
+using Microsoft.AspNet.OData.Extensions;
+using ODataComlextype.Models;
+
+namespace ODataComlextype
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            // Web API configuration and services
+
+            // Web API routes
+            config.MapHttpAttributeRoutes();
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+
+            ODataModelBuilder builder = new ODataConventionModelBuilder();
+            builder.EntitySet<Product>("Products");
+            builder.EntitySet<Book>("Books");
+            builder.EntitySet<Customer>("Customers");
+            config.MapODataServiceRoute(
+                routeName: "ODataRoute",
+                routePrefix: null,
+                model: builder.GetEdmModel());
+        }
+    }
+}
---
+++ ODataComlextype/Controllers/ProductsController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNet.OData;
+using ODataComlextype.Models;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+
+
+namespace ODataComlextype.Controllers
+{
+    public class ProductsController : ODataController
+    {
+        ProductsContext db = new ProductsContext();
+        public ProductsController()
+        {
+            Product product = new Product()
+            {
+                Id = 1,
+                Name="name",
+                Price=9,
+                Category="a"
+            };
+            db.Products.Add(product);
+            db.SaveChanges();
+        }
+        private bool ProductExists(int key)
+        {
+            return db.Products.Any(p => p.Id == key);
+        }
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
+        }
+
+        [EnableQuery]
+        public IQueryable<Product> Get()
+        {
+            var res= db.Products;
+            return res;
+
+        }
+        [EnableQuery]
+        public SingleResult<Product> Get([FromODataUri] int key)
+        {
+            IQueryable<Product> result = db.Products.Where(p => p.Id == key);
+            return SingleResult.Create(result);
+        }
+       
+    }
+}
